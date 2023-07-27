28ce2babbbdfa150707087789dccbac3fc2167d8
Martha Lang <martha@thelangs.name>
2023-07-27 13:34:30 +0100

    Initial Commit

---
+++ AboutMeApplication/AboutMeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApplicationApp.swift
+//  AboutMeApplication
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
